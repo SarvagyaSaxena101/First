d9a8d2dd9e095aa9e5322adba82b446fd122fdfe
somesh78 <91652176+somesh78@users.noreply.github.com>
2024-03-11 09:20:37 +0000

    .

--- First/settings.py
+++ First/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lvwdlhtvn6jwdb-f*b-j-lwvto-ncmw_@0vh+4i(9%zi%%$m&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
